55ba6ab0811611cfc82c4aafe4839844b1c4680a
Ayabonga <aya@westerncapelabs.com>
2017-04-02 21:49:42 +0200

    Added a Procfile

--- app.js
+++ app.js
@@ -24,3 +24,6 @@ app.get('/',function(req,res){
   res.send(pageHtml.toString())
 })
 app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
